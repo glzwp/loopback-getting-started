56142c73660375c1f0e415c0746b551c499e25e3
gl_zwp <gl_zwp@hotmail.com>
2017-01-10 20:53:56 +0800

    (1) Remove ACL for coffee-shop, message and reviewer model. That was the mistake in the getting started guide (2) Add client anular application (3) Add the code to access the model data from the route.

--- loopback-getting-started/server/boot/routes.js
+++ loopback-getting-started/server/boot/routes.js
@@ -1,7 +1,17 @@
 ﻿module.exports = function (app) {
     var router = app.loopback.Router();
+    //var coffee = app.models.CoffeeShop;
+    var reviewer = app.models.Reviewer;
     router.get('/ping', function (req, res) {
-        res.send('pongaroo');
+        reviewer.find({ where: { email: "jane@doe.com" }},function (err, instance) {
+            var response = "Password of JaneDoe is  " + instance[0].password;
+            res.send(response);
+            console.log(response);
+        });
+        //coffee.status(function (d, response) {
+        //    res.send(response);
+        //});
+        //res.send('pongaroo');
     });
     app.use(router);
 }
